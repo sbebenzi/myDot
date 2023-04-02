2f50e8f6165348f9c8e67eb5f2f11cf151c405e3
EduDegan <edegan@furb.br>
2023-04-02 15:26:35 -0300

    Routes- Login

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,11 +2,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
-import { Home } from '../screens/Home';
+import  Home  from '../screens/Home';
+import  Login  from '../Login/index';
 
 export function AppRoutes() {
     return(
     <Navigator screenOptions={ {headerShown: false}}>
+           <Screen
+            name='login'
+            component={Login}
+        />
         <Screen
             name='home'
             component={Home}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
-import { Header } from "../components/Header";
+import { Header } from "../Components/Header";
 
-export function Home(){
+export default function Home(){
     return(
         <View className="flex-1 bg-background px-8 pt-16">
             <Header/>
