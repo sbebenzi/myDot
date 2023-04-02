b46d2358dd992908497dc016ad2176fa902caec3
EduDegan <edegan@furb.br>
2023-04-02 15:32:33 -0300

    Header in Login

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,9 +1,11 @@
 import { View } from "react-native";
 import Login from "./Components/LogIn";
+import { Header } from "../Components/Header";
 
 export default function FullLogin(){
   return(
-    <View>
+    <View className='flex-1 bg-background px-8 pt-16'>
+      <Header/>
       <Login/>
     </View>
   )
