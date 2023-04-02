881f318cfe5b96f7d18a6a2d744822acea51485b
sarah benzi <sarahlover822@gmail.com>
2023-04-02 14:26:54 -0300

    feito layout do login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import {  Text, View } from 'react-native';
+import FullLogin from './src/Login';
 
 export default function App() {
   return (
     <View className= "mt-40">
-      <Text>MyDot</Text>
+     <FullLogin/>
     </View>
   );
 }
