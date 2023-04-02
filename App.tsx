3528cf6940873558d444b842278ef005e592d181
EduDegan <edegan@furb.br>
2023-04-02 15:10:01 -0300

    Routes, Initial Header, HomePage, navigation-react

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import {  Text, View } from 'react-native';
+import { Routes } from './src/routes';
+import {StatusBar} from 'react-native';
 
 export default function App() {
   return (
-    <View className= "mt-40">
-      <Text>MyDot</Text>
-    </View>
+    <>
+    <Routes/>
+    <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+  </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export function Header() {
+
+  return (
+    <View className="w-full items-center bg-background justify-center">
+        <Text className="text-white font-semibold text-base">
+            HEADER
+        </Text>
+    </View>
+  )
+}
